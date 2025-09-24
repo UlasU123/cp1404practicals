6ccb8f15b6c8f5e6e1d224e2a0ed4805e4ba55f7
ulasu <ulas.ucar@my.jcu.edu.au>
2025-09-24 19:58:22 +1000

    Add prac 1 files

---
+++ prac_01/loops.py
@@ -0,0 +1,38 @@
+
+for i in range(1, 21, 2):
+    print(i, end=' ')
+print()
+
+
+for i in range(0, 101, 10):
+    print(i, end=' ')
+print()
+
+
+for i in range(20, 0, -1):
+    print(i, end=' ')
+print()
+
+
+
+number_of_stars = int(input("Number of stars: "))
+for i in range(number_of_stars):
+    print("*", end='')
+print()
+
+
+number_of_lines = int(input("Number of lines: "))
+for i in range(1, number_of_lines + 1):
+    print("*" * i)
+
+
+
+
+
+
+
+
+
+
+
+
