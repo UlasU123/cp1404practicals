e198a77e23a965978c36d2ea718528cf560736b5
ulasu <ulas.ucar@my.jcu.edu.au>
2025-10-15 19:02:50 +1000

    add list for hex_colours.py

---
+++ prac_05/hex_colours.py
@@ -0,0 +1,22 @@
+
+
+
+
+COLOUR_NAMES = {
+    "AliceBlue": "#f0f8ff",
+    "AntiqueWhite": "#faebd7",
+    "Aqua": "#00ffff",
+    "Aquamarine": "#7fffd4",
+    "Azure": "#f0ffff",
+    "Beige": "#f5f5dc",
+    "Bisque": "#ffe4c4",
+    "Black": "#000000",
+    "BlanchedAlmond": "#ffebcd",
+    "BlueViolet": "#8a2be2"
+}
+
+
+
+
+
+
