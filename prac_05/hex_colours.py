59f152d04c5c1a23de1bf8d918cbda133f424973
ulasu <ulas.ucar@my.jcu.edu.au>
2025-10-15 19:05:48 +1000

    add rest of hex_colours.py code and completed

--- prac_05/hex_colours.py
+++ prac_05/hex_colours.py
@@ -1,7 +1,6 @@
 
 
 
-
 COLOUR_NAMES = {
     "AliceBlue": "#f0f8ff",
     "AntiqueWhite": "#faebd7",
@@ -15,8 +14,13 @@ COLOUR_NAMES = {
     "BlueViolet": "#8a2be2"
 }
 
-
-
+colour_name = input("Enter a colour name: ")
+while colour_name != "":
+    if colour_name in COLOUR_NAMES:
+        print(f"{colour_name} is {COLOUR_NAMES[colour_name]}")
+    else:
+        print("Invalid colour name")
+    colour_name = input("Enter a colour name: ")
 
 
 
