f223930cf0a4ffc0b93d591c4755a12847629fd8
ulasu <ulas.ucar@my.jcu.edu.au>
2025-11-08 11:30:03 +1000

    add guitar.py add myguitars.py

---
+++ prac_07/guitar.py
@@ -0,0 +1,27 @@
+"""
+CP1404 Practical
+Guitar class
+"""
+
+class Guitar:
+    """Represent a Guitar object."""
+
+    def __init__(self, name, year, cost):
+        """Initialise a Guitar instance."""
+        self.name = name
+        self.year = year
+        self.cost = cost
+
+    def __str__(self):
+        """Return a string representation of the Guitar."""
+        return f"{self.name} ({self.year}) : ${self.cost:,.2f}"
+
+    def get_age(self):
+        """Return how old the guitar is in years."""
+        current_year = 2025
+        return current_year - self.year
+
+    def is_vintage(self):
+        """Return True if the guitar is 50 or more years old."""
+        return self.get_age() >= 50
+
